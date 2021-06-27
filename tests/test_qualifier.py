5291bdae352d1bbfba24569b3ff203182a6660be
JuneB2021 <85796542+JuneB2021@users.noreply.github.com>
2021-06-27 13:29:13 -0500

    Add files via upload
    adding test_save_csv

--- tests/test_qualifier.py
+++ tests/test_qualifier.py
@@ -1,4 +1,5 @@
 # Import pathlib
+from os import path
 from pathlib import Path
 
 #Import fileio
@@ -14,8 +15,9 @@ from qualifier.filters import loan_to_value
 from qualifier.filters import max_loan_size
 
 def test_save_csv():
-    # @TODO: Your code here!
-    # Use Path from pathlib to output the test csv to ./data/output/qualifying_loans.csv
+    # assert from pathlib to output the test csv to ./data/output/qualifying_loans.csv
+    output_file = Path("C:/Users/jucha/FinTechWorkspace/Challenge/Module2/qualifier/data/Output/qualifying_loans.csv")
+    assert path.is_file()
 
 def test_calculate_monthly_debt_ratio():
     assert calculators.calculate_monthly_debt_ratio(1500, 4000) == 0.375
@@ -24,7 +26,7 @@ def test_calculate_loan_to_value_ratio():
     assert calculators.calculate_loan_to_value_ratio(210000, 250000) == 0.84
 
 def test_filters():
-    bank_data = fileio.load_csv(Path('./data/daily_rate_sheet.csv'))
+    bank_data = fileio.load_csv(Path('C:/Users/jucha/FinTechWorkspace/Challenge/Module2/qualifier/data/daily_rate_sheet.csv'))
     current_credit_score = 750
     debt = 1500
     income = 4000
